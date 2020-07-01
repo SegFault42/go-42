46bb9eba0efc0f05daa2bb13fcd979b93330c951
Ramzi Bouguerra <ramzi@21-school.ru>
2020-07-01 13:11:05 +0300

    add scope

--- fortyTwo.go
+++ fortyTwo.go
@@ -1,4 +1,4 @@
-package fortyTwo
+package fortytwo
 
 import (
 	"encoding/json"
@@ -7,8 +7,7 @@ import (
 	"strings"
 )
 
-var ApiToken string = ""
-var ApiURL string = "https://api.intra.42.fr/v2/"
+var APIURL string = "https://api.intra.42.fr/v2/"
 
 type sToken struct {
 	AccessToken string `json:"access_token"`
@@ -18,32 +17,30 @@ type sToken struct {
 	CreatedAt   int    `json:"created_at"`
 }
 
-func NewClient(uid, secret string) error {
-	reader := strings.NewReader(`grant_type=client_credentials&client_id=` + uid + `&client_secret=` + secret + `&scope=projects%20public%20tig`)
+func NewClient(uid, secret, scope string) (sToken, error) {
+	var tokenJSON sToken
+
+	reader := strings.NewReader(`grant_type=client_credentials&client_id=` + uid + `&client_secret=` + secret + `&scope=` + scope)
 	req, err := http.NewRequest("POST", "https://api.intra.42.fr/oauth/token", reader)
 	if err != nil {
-		return err
+		return tokenJSON, err
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err
+		return tokenJSON, err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return tokenJSON, err
 	}
-
-	var tokenJSON sToken
 
 	if err = json.Unmarshal([]byte(body), &tokenJSON); err != nil {
-		return err
+		return tokenJSON, err
 	}
 
-	ApiToken = tokenJSON.AccessToken
-
-	return nil
+	return tokenJSON, nil
 }
