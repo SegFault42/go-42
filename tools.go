26a79ee0564935e82b9211b75802b5a2ff7fd764
SegFault42 <segfault42@protonmail.com>
2021-06-21 12:19:01 +0300

    get project ID

--- tools.go
+++ tools.go
@@ -39,3 +39,34 @@ func (api APIInfo) LoginToUserID(login string) (string, error) {
 
 	return strconv.Itoa(user[0].UserID), nil
 }
+func (api APIInfo) ProjectToProjectID(project string) (string, error) {
+
+	type sProject []struct {
+		ProjectID int `json:"id"`
+	}
+
+	var proj sProject
+
+	url := "/v2/projects"
+	headers := map[string]string{
+		"Authorization": "Bearer " + os.Getenv("INTRA_TOKEN"),
+		"Content-Type":  "application/json",
+	}
+	queryParams := map[string]string{
+		"filter[slug]": project,
+	}
+
+	resp, err := api.Get(url, queryParams, headers)
+	if err != nil {
+		return "", err
+	}
+
+	if err = json.Unmarshal([]byte(resp.Body), &proj); err != nil {
+		return "", err
+	}
+	if len(proj) == 0 {
+		return "", errors.New("Project not found")
+	}
+
+	return strconv.Itoa(proj[0].ProjectID), nil
+}
